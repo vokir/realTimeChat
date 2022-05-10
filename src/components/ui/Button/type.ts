4ce46bab177f1abdcf4f2a93c0ed578f1ef08364
Владимир <64137814+vokir@users.noreply.github.com>
2022-05-10 18:07:38 +0500

    Delete src directory

--- src/components/ui/Button/type.ts
+++
@@ -1,7 +0,0 @@
-export interface IButtonProps {
-    disabled?: boolean,
-    loading?: boolean,
-    href?: string,
-    classes?: string,
-    children?: React.ReactNode,
-}
--- src/components/ui/Input/type.ts
+++
@@ -1,7 +0,0 @@
-import { InputHTMLAttributes } from "react";
-
-export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-    name: string,
-    label: string,
-    classes?: string,
-}
